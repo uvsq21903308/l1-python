7e543dc53d629a7a81effcc515e326ec3a5782d9
Arnaud GAY <arnaud.gay@ens.uvsq.fr>
2020-10-16 14:51:37 +0200

    count_matt

--- exercises/hello/hello.py
+++ exercises/hello/hello.py
@@ -1,4 +1,10 @@
-msg = "hello world"
-print(msg.capitalize())
-print("Alexis le dog, ouf ouf ouf")
-print("chien Alexis chien Alexis")
+import string
+
+def cout_words(chaine):
+    m=0
+    for k in range(1, len(chaine) - 1):
+        if chaine[k] == " " and chaine[k+1] != " " and chaine[k+1] not in string.punctuation:
+            m  += 1
+    return m + 1
+
+cout_words("Incroyable cette émission, tellement heureux que Trashtalk prenne de la notoriété !")
