212b63acce969d1c32681e721a24b4f7896e6fe4
Arnaud GAY <arnaud.gay@ens.uvsq.fr>
2020-11-19 09:59:38 +0100

    td4

--- exercises/TD04_listes/syracuse.py
+++ exercises/TD04_listes/syracuse.py
@@ -1,5 +1,28 @@
 def syracuse(n):
     """ Retourne la liste des valeurs de la suite en partant de n jusqu'à 1 """
-    pass
+    liste_syracuse = []
+    while n != 1:
+        if n % 2 == 0:
+            n = n / 2
+        else:
+            n = n * 3 + 1
+        liste_syracuse.append(n)
+    return liste_syracuse        
 
-print(syracuse(3))
+
+# print(syracuse(3))
+
+def testeConjecture(n_max):
+    """ Teste la conjecture de Collatz pour toutes les valeurs de 2 à n_max """
+    for i in range(2,n_max + 1):
+        list_test = syracuse
+        while list_test[-1] == 1:
+            syracuse(i)
+            list_test = syracuse
+    x = True
+    return x 
+    
+    
+
+
+print(testeConjecture(10000))
