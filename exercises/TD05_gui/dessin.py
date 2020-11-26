87bd117c5b2d2be6be943de9b587a84efd2d5029
Arnaud GAY <arnaud.gay@ens.uvsq.fr>
2020-11-27 00:25:55 +0100

    td5 2/6

--- exercises/TD05_gui/dessin.py
+++ exercises/TD05_gui/dessin.py
@@ -1,12 +1,24 @@
 import tkinter as tk
+import random
+
+def desCercle():
+    x0 = 500
+    y0 = 500
+    x1 = 500
+    y1 =  500
+    canvas.create_oval(x0, y0, x1, y1, fill="blue")
+    return
+
+
+
 
 racine = tk.Tk()
 racine.title("Mon dessin")
-canvas = tk.Canvas(racine, bg="black")
-choix_couleur = tk.Button(racine, text="Choisir une couleur")
-cercle = tk.Button(racine, text="Cercle")
-carre = tk.Button(racine, text="Carré")
-croix = tk.Button(racine, text="Croix")
+canvas = tk.Canvas(racine, bg="black", bd=302, relief="flat")
+choix_couleur = tk.Button(racine, text="Choisir une couleur", bg="tomato3", padx=10, pady=20, font=("30"))
+cercle = tk.Button(racine, text="Cercle", padx=20, bg="Royalblue2", font=("30"), command=desCercle)
+carre = tk.Button(racine, text="Carré", padx=20, bg="orange", font=("30"))
+croix = tk.Button(racine, text="Croix", padx=20, bg="gold", font=("30"))
 
 canvas.grid(column=1, row=1, rowspan=3)
 choix_couleur.grid(column=1, row=0)
